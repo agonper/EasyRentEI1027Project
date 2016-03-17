a9b782a80d37bcc7229f6ef5413ad74446f26fd7
Alberto Gonzalez <al286290@uji.es>
2016-03-17 17:22:48 +0100

    User roles and User class skeleton created

--- src/models/transfer_objects/User.java
+++ src/models/transfer_objects/User.java
@@ -1,7 +1,34 @@
 package models.transfer_objects;
 
+import java.sql.Date;
+import java.util.List;
+
 /**
  * Created by alberto on 17/03/16.
  */
 public class User {
+    public String username;
+    public String DNI;
+    public UserRoles role;
+    public String password;
+    public String name;
+    public String surname;
+    public String email;
+    public String phoneNumber;
+    public String postalAddress;
+    public String country;
+    public String postCode;
+    public Date signUpDate;
+    public Boolean active;
+    public Date deactivatedSince;
+
+    public List<BookingProposal> getBookingProposals() {
+        // TODO: Implement
+        return null;
+    }
+
+    public List<Property> getProperties() {
+        // TODO: Implement
+        return null;
+    }
 }
