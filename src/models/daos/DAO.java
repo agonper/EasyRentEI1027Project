569e0efaf50732583e68b8b6bd282d4361ae0d45
Alberto Gonzalez <al286290@uji.es>
2016-03-19 18:46:21 +0100

    Layer-up refactor on UserDAO

--- src/models/daos/DAO.java
+++ src/models/daos/DAO.java
@@ -1,25 +1,132 @@
 package models.daos;
 
 import connectors.ConnectionManager;
+import models.common.Model;
+import models.common.Store;
+import models.transfer_objects.User;
 
 import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 /**
  * Created by Alberto on 18/03/2016.
  */
-public abstract class DAO {
+public abstract class DAO<T extends Model> implements Store<T> {
     protected static Logger log;
 
     public DAO(String className) {
         log = Logger.getLogger(className);
     }
 
-    protected Connection getConnection() throws ConnectException {
+    /**
+     * =============
+     * FUNCTIONALITY
+     * =============
+     */
+
+    @Override
+    public Set<T> findAllRecords() {
+        HashSet<T> records = new HashSet<>();
+        try {
+            Connection connection = getConnection();
+            retrieveAllRecordsFromDB(records, connection);
+        } catch (ConnectException e) {
+            log.severe("Connection error");
+            e.printStackTrace();
+        }
+        return records;
+    }
+
+    protected abstract void retrieveAllRecordsFromDB(HashSet<T> records, Connection connection);
+
+    @Override
+    public T findRecordByID(UUID id) {
+        try {
+            Connection connection = getConnection();
+            return retrieveRecordByIDFromDB(id, connection);
+        } catch (ConnectException e) {
+            log.severe("Connection error");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected abstract T retrieveRecordByIDFromDB(UUID id, Connection connection);
+
+    @Override
+    public T storeRecord(T record) {
+        record.id = UUID.randomUUID();
+        try {
+            Connection connection = getConnection();
+            insertRecordIntoDB(record, connection);
+        } catch (ConnectException e) {
+            log.severe("Connection error");
+            e.printStackTrace();
+        }
+        return record;
+    }
+
+    protected abstract void insertRecordIntoDB(T record, Connection connection);
+
+    @Override
+    public T updateRecord(T record) {
+        try {
+            Connection connection = getConnection();
+            updateDBRecord(record, connection);
+        } catch (ConnectException e) {
+            log.severe("Connection error");
+            e.printStackTrace();
+        }
+        return record;
+    }
+
+    protected abstract void updateDBRecord(T record, Connection connection);
+
+    @Override
+    public void destroyRecord(T record) {
+        try {
+            Connection connection = getConnection();
+            removeDBRecord(record, connection);
+        } catch (ConnectException e) {
+            log.severe("Connection error");
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void removeDBRecord(T record, Connection connection);
+
+    /**
+     * ===============
+     * UTILITY METHODS
+     * ===============
+     */
+
+    protected static String getInsertValuesPlaceholdersFrom(String[] fields) {
+        StringBuilder placeholders = new StringBuilder();
+        placeholders.append("?");
+        for (int i = 1; i < fields.length; i++) {
+            placeholders.append(", ?");
+        }
+        return placeholders.toString();
+    }
+
+    protected static String getUpdateFieldsFrom(String[] fields) {
+        StringBuilder updateFields = new StringBuilder();
+        updateFields.append(fields[0] + " = ?");
+        for (int i = 1; i < fields.length; i++) {
+            updateFields.append(", " + fields[i] + " = ?");
+        }
+        return updateFields.toString();
+    }
+
+    private static Connection getConnection() throws ConnectException {
         return ConnectionManager.getConnection();
     }
 
@@ -42,6 +149,4 @@ public abstract class DAO {
             }
         }
     }
-
-    // TODO: Implement operation cleanup common functions
 }
--- src/models/daos/UserDAO.java
+++ src/models/daos/UserDAO.java
@@ -1,23 +1,20 @@
 package models.daos;
 
-import models.common.Store;
 import models.transfer_objects.RoleNotFoundException;
 import models.transfer_objects.User;
 import models.transfer_objects.UserRole;
 
-import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 /**
  * Created by alberto on 17/03/16.
  */
-public class UserDAO extends DAO implements Store<User> {
+public class UserDAO extends DAO<User> {
 
     private final static String TABLE_COLUMNS = "id, username, national_document, role, password, " +
             "name, surnames, email, phone_number, country, post_address, post_code, " +
@@ -36,73 +33,7 @@ public class UserDAO extends DAO implements Store<User> {
      */
 
     @Override
-    public Set<User> findAllRecords() {
-        HashSet<User> users = new HashSet<>();
-        try {
-            Connection connection = getConnection();
-            retrieveAllUsers(users, connection);
-        } catch (ConnectException e) {
-            log.severe("Connection error");
-            e.printStackTrace();
-        }
-        return users;
-    }
-
-    @Override
-    public User findRecordByID(UUID id) {
-        try {
-            Connection connection = getConnection();
-            return retrieveUserByID(id, connection);
-        } catch (ConnectException e) {
-            log.severe("Connection error");
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public User storeRecord(User record) {
-        record.id = UUID.randomUUID();
-        try {
-            Connection connection = getConnection();
-            insertUserRecord(record, connection);
-        } catch (ConnectException e) {
-            log.severe("Connection error");
-            e.printStackTrace();
-        }
-        return record;
-    }
-
-    @Override
-    public User updateRecord(User record) {
-        try {
-            Connection connection = getConnection();
-            updateUserEntry(record, connection);
-        } catch (ConnectException e) {
-            log.severe("Connection error");
-            e.printStackTrace();
-        }
-        return record;
-    }
-
-    @Override
-    public void destroyRecord(User record) {
-        try {
-            Connection connection = getConnection();
-            removeUserRecord(record, connection);
-        } catch (ConnectException e) {
-            log.severe("Connection error");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * ===============
-     * SUPPORT METHODS
-     * ===============
-     */
-
-    private void retrieveAllUsers(HashSet<User> users, Connection connection) {
+    protected void retrieveAllRecordsFromDB(HashSet<User> users, Connection connection) {
         PreparedStatement stmt = null;
         ResultSet rs = null;
         try {
@@ -118,16 +49,8 @@ public class UserDAO extends DAO implements Store<User> {
         }
     }
 
-    private void populateUsersSet(HashSet<User> users, ResultSet rs) throws SQLException {
-        while (rs.next()) {
-            User user = populateUserModelWith(rs);
-            if (user != null) {
-                users.add(user);
-            }
-        }
-    }
-
-    private User retrieveUserByID(UUID id, Connection connection) {
+    @Override
+    protected User retrieveRecordByIDFromDB(UUID id, Connection connection) {
         User user = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -148,6 +71,77 @@ public class UserDAO extends DAO implements Store<User> {
             cleanupResources(stmt, rs);
         }
         return user;
+    }
+
+    @Override
+    protected void insertRecordIntoDB(User user, Connection connection) {
+        PreparedStatement stmt = null;
+        try {
+            String template = "INSERT INTO %s (%s) values (%s)";
+            String placeholders = getInsertValuesPlaceholdersFrom(TABLE_COLUMNS.split(", "));
+            stmt = connection.prepareStatement(String.format(template, TABLE_NAME, TABLE_COLUMNS, placeholders));
+            stmt.setObject(1, user.id);
+            final int INNITIAL_POSITION = 2;
+            setUserVariableAttributes(user, stmt, INNITIAL_POSITION);
+
+            stmt.execute();
+        } catch (SQLException e) {
+            log.severe("Error storing new user");
+            e.printStackTrace();
+        } finally {
+            cleanupResources(stmt, null);
+        }
+    }
+
+    @Override
+    protected void updateDBRecord(User user, Connection connection) {
+        PreparedStatement stmt = null;
+        try {
+            String template = "UPDATE %s SET %s WHERE id = ?";
+            String[] tableFields = TABLE_COLUMNS.split(", ");
+            String fieldsToUpdate = getUpdateFieldsFrom(tableFields);
+
+            stmt = connection.prepareStatement(String.format(template, TABLE_NAME, fieldsToUpdate));
+            setUserVariableAttributes(user, stmt, 1);
+            stmt.setObject(tableFields.length-1, user.id);
+            stmt.execute();
+        } catch (SQLException e) {
+            log.severe("Couldn't update user with id: " + user.id);
+            e.printStackTrace();
+        } finally {
+            cleanupResources(stmt, null);
+        }
+    }
+
+    @Override
+    protected void removeDBRecord(User record, Connection connection) {
+        PreparedStatement stmt = null;
+        try {
+            String template = "DELETE FROM %s WHERE id = ?";
+            stmt = connection.prepareStatement(String.format(template, TABLE_NAME));
+            stmt.setObject(1, record.id);
+            stmt.execute();
+        } catch (SQLException e) {
+            log.severe("Error deleting user with id: " + record.id);
+            e.printStackTrace();
+        } finally {
+            cleanupResources(stmt, null);
+        }
+    }
+
+    /**
+     * ===============
+     * UTILITY METHODS
+     * ===============
+     */
+
+    private void populateUsersSet(HashSet<User> users, ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            User user = populateUserModelWith(rs);
+            if (user != null) {
+                users.add(user);
+            }
+        }
     }
 
     private User populateUserModelWith(ResultSet rs) throws SQLException {
@@ -177,34 +171,6 @@ public class UserDAO extends DAO implements Store<User> {
         return user;
     }
 
-    private void insertUserRecord(User user, Connection connection) {
-        PreparedStatement stmt = null;
-        try {
-            String template = "INSERT INTO %s (%s) values (%s)";
-            String placeholders = getInsertValuesPlaceholdersFrom(TABLE_COLUMNS.split(", "));
-            stmt = connection.prepareStatement(String.format(template, TABLE_NAME, TABLE_COLUMNS, placeholders));
-            stmt.setObject(1, user.id);
-            final int INNITIAL_POSITION = 2;
-            setUserVariableAttributes(user, stmt, INNITIAL_POSITION);
-
-            stmt.execute();
-        } catch (SQLException e) {
-            log.severe("Error storing new user");
-            e.printStackTrace();
-        } finally {
-            cleanupResources(stmt, null);
-        }
-    }
-
-    private String getInsertValuesPlaceholdersFrom(String[] fields) {
-        StringBuilder placeholders = new StringBuilder();
-        placeholders.append("?");
-        for (int i = 1; i < fields.length; i++) {
-            placeholders.append(", ?");
-        }
-        return placeholders.toString();
-    }
-
     private void setUserVariableAttributes(User user, PreparedStatement stmt, int p) throws SQLException {
         stmt.setString(p++, user.username);
         stmt.setString(p++, user.DNI);
@@ -220,46 +186,5 @@ public class UserDAO extends DAO implements Store<User> {
         stmt.setDate(p++, user.signUpDate);
         stmt.setBoolean(p++, user.active);
         stmt.setDate(p, user.deactivatedSince);
-    }
-
-    private void updateUserEntry(User user, Connection connection) {
-        PreparedStatement stmt = null;
-        try {
-            String template = "UPDATE %s SET %s WHERE id = ?";
-            String[] tableFields = TABLE_COLUMNS.split(", ");
-            String fieldsToUpdate = getUpdateFields(tableFields);
-
-            stmt = connection.prepareStatement(String.format(template, TABLE_NAME, fieldsToUpdate));
-            setUserVariableAttributes(user, stmt, 1);
-            stmt.setObject(tableFields.length-1, user.id);
-            stmt.execute();
-        } catch (SQLException e) {
-            log.severe("Couldn't update user with id: " + user.id);
-            e.printStackTrace();
-        } finally {
-            cleanupResources(stmt, null);
-        }
-    }
-
-    private String getUpdateFields(String[] fields) {
-        StringBuilder updateFields = new StringBuilder();
-        updateFields.append(fields[0] + " = ?");
-        for (int i = 1; i < fields.length; i++) {
-            updateFields.append(", " + fields[i] + " = ?");
-        }
-        return updateFields.toString();
-    }
-
-    private void removeUserRecord(User record, Connection connection) {
-        PreparedStatement stmt = null;
-        try {
-            String template = "DELETE FROM %s WHERE id = ?";
-            stmt = connection.prepareStatement(String.format(template, TABLE_NAME));
-            stmt.setObject(1, record.id);
-            stmt.execute();
-        } catch (SQLException e) {
-            log.severe("Error deleting user with id: " + record.id);
-            e.printStackTrace();
-        }
     }
 }
