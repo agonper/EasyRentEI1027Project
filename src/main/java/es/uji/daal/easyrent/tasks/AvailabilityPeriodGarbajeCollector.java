a36a52d867b1fe71ff1ee2c16412ca4ad838856a
Alberto Gonzalez <al286290@uji.es>
2016-06-19 12:37:12 +0200

    Now we avoid owners to add an availability where there is a valid booking proposal

--- src/main/java/es/uji/daal/easyrent/tasks/AvailabilityPeriodGarbajeCollector.java
+++ src/main/java/es/uji/daal/easyrent/tasks/AvailabilityPeriodGarbajeCollector.java
@@ -1,18 +1,11 @@
 package es.uji.daal.easyrent.tasks;
 
-import es.uji.daal.easyrent.model.AvailabilityPeriod;
-import es.uji.daal.easyrent.model.BookingProposal;
 import es.uji.daal.easyrent.repository.AvailabilityPeriodRepository;
-import es.uji.daal.easyrent.repository.BookingProposalRepository;
-import es.uji.daal.easyrent.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  * Created by Alberto on 17/06/2016.
